b7f816e9cf47f53dca4f7fdc24590ad2d2d57cce
NguyenHopThanh <112919532+NguyenHopThanh@users.noreply.github.com>
2023-05-18 10:51:19 +0700

    Update db.php for gpdg pmticket

--- inc/db.php
+++ inc/db.php
@@ -6,6 +6,6 @@ if (!defined('APPLICATION_LOADED') || !APPLICATION_LOADED) {
 
 //Database config
 define('HOST', 'localhost');
-define('USER', 'u848302103_pmtiket');
+define('USER', 'u848302103_pmticket');
 define('PASS', '1qazXSW@');
-define('DATABASE', 'u848302103_namevn_ist');
+define('DATABASE', 'u848302103_gpdg_its');
