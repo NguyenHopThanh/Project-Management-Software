aae8fe842f8fec6b90653335def7c78cce0b6b6b
NguyenHopThanh <112919532+NguyenHopThanh@users.noreply.github.com>
2023-05-18 12:28:02 +0700

    Update home.php

--- _html_parts/home.php
+++ _html_parts/home.php
@@ -10,7 +10,7 @@ $projects = $this->getProjects();
 <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
 <div id="content">
     <div id="home" class="container-fluid">
-        <h1><?= $this->lang_php['welcome_to'] ?> NHT's EOL Tracking System <!--<?= $_SERVER['HTTP_HOST'] ?>--></h1>
+        <h1><?= $this->lang_php['welcome_to'] ?> Project Management System<!--<?= $_SERVER['HTTP_HOST'] ?>--></h1>
         <div class="nav-top-content">
             <?php if (in_array($GLOBALS['CONFIG']['PERMISSIONS']['CREATE_PROJECT'], $this->permissions) || in_array($GLOBALS['CONFIG']['PERMISSIONS']['SETTINGS_PAGE'], $this->permissions)) { ?>
                 <a href="javascript:void(0);" data-toggle="modal" data-target="#createModal" data-tooltip="tooltip" data-placement="bottom" title="<?= $this->lang_php['create_new_project'] ?>"><i class="fa fa-plus-square"></i></a>
